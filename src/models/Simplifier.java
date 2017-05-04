83f37d7f30dd677d029713c68e71fe8d3a051d1f
youmna_dwidar <youmnadwidar@hotmail.com>
2017-05-04 21:02:18 +0200

    attributes

---
+++ src/models/Simplifier.java
@@ -0,0 +1,17 @@
+package models;
+
+import java.util.LinkedList;
+
+public class Simplifier implements ISimplifier {
+	boolean [][] checked;
+	int [] minterms;
+	LinkedList<MintermReduced> answer =new LinkedList<>();
+
+	@Override
+	public LinkedList<MintermReduced> simplify(LinkedList<MintermReduced> terms, int literals) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
