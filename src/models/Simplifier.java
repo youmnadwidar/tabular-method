b74c7f1bf66786c8825c5fe0542bff4943411fba
youmna_dwidar <youmnadwidar@hotmail.com>
2017-05-09 20:33:22 +0200

    some mistakes were fixed.something wrong with recursion trying to fix it

--- src/models/Simplifier.java
+++ src/models/Simplifier.java
@@ -16,12 +16,12 @@ public class Simplifier implements ISimplifier {
 	int[] wantedTerms;
 
 	@Override
-	public LinkedList<LinkedList<MintermReduced>> simplify(
-			LinkedList<MintermReduced> firststep, int bits,
+	public LinkedList<LinkedList<MintermReduced>> simplify(LinkedList<MintermReduced> firststep, int bits,
 			int[] wantedTerms) {
 		this.bits = bits;
 		this.wantedTerms = wantedTerms;
 		Reducedterms = firststep;
+		minterms = new int[(int) Math.pow(2, bits)];
 		fill2Darray(bits);
 		getEssential(wantedTerms);
 		getRowDominant();
@@ -31,53 +31,39 @@ public class Simplifier implements ISimplifier {
 
 	public void getRowDominant() {
 		int i = Reducedterms.size() - 1;
-		while (i >= 0) {
-			if (getcoverTerms(Reducedterms
-					.get(i)).length > getcoverTerms(
-							Reducedterms
-									.get(i - 1)).length) {
+		while (i > 0) {
+			if (getcoverTerms(Reducedterms.get(i)).length > getcoverTerms(Reducedterms.get(i - 1)).length) {
 				answer.add(Reducedterms.get(i));
 				remove(Reducedterms.get(i));
 			} else {
 				LinkedList<MintermReduced> temp = new LinkedList<>();
-				while (getcoverTerms(Reducedterms
-						.get(i)).length == getcoverTerms(
-								Reducedterms.get(
-										i - 1)).length) {
+				while (i > 0
+						&& getcoverTerms(Reducedterms.get(i)).length == getcoverTerms(Reducedterms.get(i - 1)).length) {
 					temp.add(Reducedterms.get(i));
 					i--;
 				}
 				temp.add(Reducedterms.get(i));
-				MintermReduced term = getColoumnDominant(
-						temp);
+				MintermReduced term = getColoumnDominant(temp);
 				if (term != null) {
 					answer.add(term);
 					remove(term);
 				} else {
 					answers.add(answer);
-					MintermReduced first = Reducedterms
-							.get(i);
+					MintermReduced first = Reducedterms.get(i);
 					Reducedterms.remove(i);
-					LinkedList<LinkedList<MintermReduced>> path1 = simplify(
-							Reducedterms, bits,
-							wantedTerms);
+					LinkedList<LinkedList<MintermReduced>> path1 = simplify(Reducedterms, bits, wantedTerms);
 					Reducedterms.add(i, first);
-					MintermReduced second = Reducedterms
-							.get(i - 1);
+					MintermReduced second = Reducedterms.get(i - 1);
 					Reducedterms.remove(i - 1);
-					LinkedList<LinkedList<MintermReduced>> path2 = simplify(
-							Reducedterms, bits,
-							wantedTerms);
-					converge(answers,
-							converge(path1, path2));
+					LinkedList<LinkedList<MintermReduced>> path2 = simplify(Reducedterms, bits, wantedTerms);
+					converge(answers, converge(path1, path2));
 				}
 			}
 		}
 
 	}
 
-	private LinkedList<LinkedList<MintermReduced>> converge(
-			LinkedList<LinkedList<MintermReduced>> path1,
+	private LinkedList<LinkedList<MintermReduced>> converge(LinkedList<LinkedList<MintermReduced>> path1,
 			LinkedList<LinkedList<MintermReduced>> path2) {
 		for (int i = 0; i < path2.size(); i++) {
 			path1.add(path2.get(i));
@@ -85,16 +71,14 @@ public class Simplifier implements ISimplifier {
 		return path1;
 	}
 
-	public MintermReduced getColoumnDominant(
-			LinkedList<MintermReduced> terms) {
+	public MintermReduced getColoumnDominant(LinkedList<MintermReduced> terms) {
 		int maxColoumnIndex = 0;
 		int prevCount = 0;
 
 		for (int i = 0; i < terms.size(); i++) {
-			int[] coveredTerms = getcoverTerms(
-					terms.get(i));
+			int[] coveredTerms = getcoverTerms(terms.get(i));
 			int termsCount = 0;
-			for (int j = 0; j < terms.size(); j++) {
+			for (int j = 0; j < coveredTerms.length; j++) {
 				termsCount += minterms[coveredTerms[j]];
 			}
 			if (termsCount > prevCount) {
@@ -104,18 +88,18 @@ public class Simplifier implements ISimplifier {
 
 				return null;
 			}
+			prevCount=termsCount;
+			
 		}
 		return terms.get(maxColoumnIndex);
 	}
 
 	public void fill2Darray(int bits) {
+
 		Arrays.fill(minterms, 0);
-		checked = new boolean[Reducedterms
-				.size()][(int) Math.pow(2, bits)];
+		checked = new boolean[Reducedterms.size()][(int) Math.pow(2, bits)];
 		for (int i = 0; i < Reducedterms.size(); i++) {
-			int[] coverd = new int[(int) Math.pow(2,
-					Reducedterms.get(i)
-							.getReducedDifferences().length)];
+			int[] coverd = new int[(int) Math.pow(2, Reducedterms.get(i).getReducedDifferences().length)];
 			coverd = getcoverTerms(Reducedterms.get(i));
 			for (int j = 0; j < coverd.length; j++) {
 
@@ -126,22 +110,21 @@ public class Simplifier implements ISimplifier {
 	}
 
 	public void getEssential(int[] wantedTerms) {
-		int[] essentials = new int[Reducedterms.size()];
+		LinkedList<Integer> essentials = new LinkedList<>();;
 		int j = 0;
 		for (int i = 0; i < minterms.length; i++) {
 			if (minterms[i] == 1) {
 				if (contain(wantedTerms, i)) {
-					essentials[j] = i;
-					j++;
+					essentials.add(i);
 				}
 			}
 		}
 		for (int i = 0; i < Reducedterms.size(); i++) {
-			for (int k = 0; k < essentials.length; k++) {
+			for (int k = 0; k < essentials.size(); k++) {
 
 				if (checked[i][k] == true) {
 					answer.add(Reducedterms.get(i));
-					minterms[essentials[k]]--;
+					minterms[essentials.get(k)]--;
 					continue;
 				}
 
@@ -162,8 +145,7 @@ public class Simplifier implements ISimplifier {
 		answer.remove(term);
 	}
 
-	public static void powerset(int[] s, int i, int k,
-			int[] buff) {
+	public static void powerset(int[] s, int i, int k, int[] buff) {
 		if (i < k) {
 			for (int j = i; j < s.length; j++) {
 				int o = s[j];
@@ -193,22 +175,16 @@ public class Simplifier implements ISimplifier {
 
 	public int[] getcoverTerms(MintermReduced term) {
 		diff = new LinkedList<>();
-		int[] coveredTerms = new int[(int) Math.pow(2,
-				term.getReducedDifferences().length)];
-		for (int j2 = 1; j2 <= term
-				.getReducedDifferences().length; j2++) {
+		int[] coveredTerms = new int[(int) Math.pow(2, term.getReducedDifferences().length)];
+		for (int j2 = 1; j2 <= term.getReducedDifferences().length; j2++) {
 
-			powerset(term.getReducedDifferences(), 0, j2,
-					new int[term
-							.getReducedDifferences().length]);
+			powerset(term.getReducedDifferences(), 0, j2, new int[term.getReducedDifferences().length]);
 		}
 		int j = 1;
 		coveredTerms[0] = term.getValue();
 		for (int i = 0; i < diff.size(); i++) {
-			if (!contain(coveredTerms, term.getValue()
-					+ IntStream.of(diff.get(i)).sum())) {
-				coveredTerms[j] = term.getValue()
-						+ IntStream.of(diff.get(i)).sum();
+			if (!contain(coveredTerms, term.getValue() + IntStream.of(diff.get(i)).sum())) {
+				coveredTerms[j] = term.getValue() + IntStream.of(diff.get(i)).sum();
 				j++;
 			}
 		}
@@ -217,10 +193,8 @@ public class Simplifier implements ISimplifier {
 	}
 
 	/**
-	 * @return 1 when the second array is a subset of the first
-	 *         array.
-	 * @return -1 when the first array is a subset of the second
-	 *         array
+	 * @return 1 when the second array is a subset of the first array.
+	 * @return -1 when the first array is a subset of the second array
 	 * @return 0 when no array is subset of another
 	 */
 	public int containsAll(int[] a, int[] b) {
