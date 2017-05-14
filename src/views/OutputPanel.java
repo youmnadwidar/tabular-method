4b4c38799309336e79eb3fa6b092c447e469897e
Mario_Hunter <mario.hunter.std@alexu.edu.eg>
2017-05-14 22:22:09 +0200

    scroll bar

--- src/views/OutputPanel.java
+++ src/views/OutputPanel.java
@@ -24,6 +24,8 @@ public class OutputPanel extends JPanel {
 	private JTextPane status;
 	private JTextPane result;
 	private JTextPane steps;
+	JScrollPane scrPane_2;
+	JScrollPane scrPane_1;
 
 	/**
 	 * Create the panel.
@@ -34,11 +36,7 @@ public class OutputPanel extends JPanel {
 
 		JPanel panel = new JPanel();
 		add(panel);
-		JScrollPane scrPane = new JScrollPane(panel);
-		scrPane.setEnabled(true);
-		scrPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
-		add(scrPane);
-		panel.setForeground(SystemColor.textHighlight);
+		 		panel.setForeground(SystemColor.textHighlight);
 		panel.setBackground(new Color(255, 255, 204));
 		GridBagLayout gbl_panel = new GridBagLayout();
 		gbl_panel.columnWidths = new int[] {42, 161, 1, 260,
@@ -82,7 +80,7 @@ public class OutputPanel extends JPanel {
 				.setFont(new Font("Tahoma", Font.BOLD, 11));
 
 		result = new JTextPane();
-		panel_1.add(result, BorderLayout.CENTER);
+		//panel_1.add(result, BorderLayout.CENTER);
 		result.setForeground(SystemColor.activeCaption);
 		result.setFont(new Font("Tahoma",
 				Font.BOLD | Font.ITALIC, 11));
@@ -105,8 +103,7 @@ public class OutputPanel extends JPanel {
 		lblsteps.setForeground(SystemColor.textHighlight);
 		lblsteps.setFont(new Font("Tahoma", Font.BOLD, 11));
 
-		JScrollPane scrollPane_1 = new JScrollPane();
-		panel_2.add(scrollPane_1, BorderLayout.EAST);
+		
 
 		steps = new JTextPane();
 		steps.setForeground(SystemColor.activeCaption);
@@ -114,8 +111,20 @@ public class OutputPanel extends JPanel {
 				Font.BOLD | Font.ITALIC, 11));
 		steps.setEditable(false);
 		steps.setBackground(new Color(235, 235, 204));
+		
+		scrPane_1 = new JScrollPane(result);
+		scrPane_1.setEnabled(true);
+		scrPane_1.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+	
+		panel_1.add(scrPane_1,BorderLayout.CENTER);
+		
+		scrPane_2 = new JScrollPane(steps);
+		scrPane_2.setEnabled(true);
+		scrPane_2.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+		panel_2.add(scrPane_2,BorderLayout.CENTER);
+
 		result.setAutoscrolls(true);
-		panel_2.add(steps, BorderLayout.CENTER);
+		//panel_2.add(steps, BorderLayout.CENTER);
 		GridBagConstraints gbc_panel_2 = new GridBagConstraints();
 		gbc_panel_2.fill = GridBagConstraints.BOTH;
 		gbc_panel_2.gridwidth = 4;
@@ -135,6 +144,8 @@ public class OutputPanel extends JPanel {
 
 	public void setSteps(String text) {
 		steps.setText(text);
+        scrPane_2.getVerticalScrollBar().setValue(scrPane_2.getVerticalScrollBar().getMaximum());
+
 	}
 
 	public void viewSteps(boolean choice) {
